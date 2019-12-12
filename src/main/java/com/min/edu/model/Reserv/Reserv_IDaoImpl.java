8e1ac79fbbb751576454dd0bf1dddbd9fa5f06dc
EJ <reueunji@gmail.com>
2019-12-12 17:52:50 +0900

    Reser_Mapper 작성 및 Reser_Dao, Reser_Service, Reser_Controller, Reser_Dto, reser.jsp, Configuration, Hospi_Dto 수정, Ani_Dto, Obj_Dto 작성

--- src/main/java/com/min/edu/model/Reserv/Reserv_IDaoImpl.java
+++ src/main/java/com/min/edu/model/Reserv/Reserv_IDaoImpl.java
@@ -1,5 +1,32 @@
 package com.min.edu.model.Reserv;
 
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import com.min.edu.dto.Obj_Dto;
+
+@Repository
 public class Reserv_IDaoImpl implements Reserv_IDao {
+	
+	@Autowired
+	SqlSessionTemplate session;
+	
+	String NS = "sarang.hagae.reser.";
+
+	@Override
+	public List<Obj_Dto> selectObj(String h_regi) {
+		
+		return session.selectList(NS+"selectObj",h_regi);
+	}
+
+	@Override
+	public List<Obj_Dto> SelectOBJ() {
+		
+		return session.selectList(NS+"SelectOBJ");
+	}
 
 }
